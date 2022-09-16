0cbf3c91f0ec3ad3098abf94d1828e27ffb7a043
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-16 14:39:35 +0200

    Search engine

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Dictionary from "./Dictionary";
 
 export default function App() {
   return (
@@ -7,6 +8,12 @@ export default function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer>
+        <small>Coded by Amanda Machado</small>
+      </footer>
     </div>
   );
 }
