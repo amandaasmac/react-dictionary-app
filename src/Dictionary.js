9f26ccac732ae8efd95240a6d47e259d228a6c28
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-19 18:29:40 +0200

    Photos

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,14 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.default);
   let [searchData, setSearchData] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
   function handleKeywordChange(event) {
     setKeyword(event.target.value);
@@ -14,7 +16,12 @@ export default function Dictionary(props) {
 
   function search() {
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    const pexelsApiKey = `563492ad6f9170000100000121a3289e7cd5471d9e39a7a5768a496a`;
+    const pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    const headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsUrl, { headers: headers }).then(handlePexelsResponse);
   }
 
   function handleSubmit(event) {
@@ -22,8 +29,13 @@ export default function Dictionary(props) {
     search();
   }
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setSearchData(response.data[0]);
+  }
+
+  function handlePexelsResponse(response) {
+    console.log(response);
+    setPhotos(response.data.photos);
   }
 
   function load() {
@@ -45,6 +57,7 @@ export default function Dictionary(props) {
           </form>
         </header>
         <Results data={searchData} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
---
+++ src/Photos.js
@@ -0,0 +1,20 @@
+import React from "react";
+import "./Photos.css";
+
+export default function Photos(props) {
+  if (props.photos) {
+    return (
+      <div className="Photos">
+        {props.photos.map(function (photo, index) {
+          return (
+            <a href={photo.src.original} target="_blank" rel="noreferrer">
+              <img src={photo.src.landscape} key={index} alt="" />
+            </a>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return null;
+  }
+}
