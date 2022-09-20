bb558714e5c4a5d453ceda7378d749648d39baa0
amandaasmac <108792107+amandaasmac@users.noreply.github.com>
2022-09-20 09:41:59 +0200

    uninstall ReactAudioPlayer

--- src/Phonetic.js
+++ src/Phonetic.js
@@ -1,15 +1,20 @@
 import React from "react";
-import ReactAudioPlayer from "react-audio-player";
+
 import "./Phonetic.css";
 
 export default function Phonetic(props) {
-  return (
-    <div className="Phonetic">
-      <p>
-        <em>{props.phonetic.text}</em>
-      </p>
+  if (props.phonetic) {
+    return (
+      <div className="Phonetic">
+        <p>
+          <em>{props.phonetic.text}</em>
+        </p>
+        <i class="fa-solid fa-volume-high"></i>
 
-      <ReactAudioPlayer src={props.phonetic.audio} controls />
-    </div>
-  );
+        {/* <ReactAudioPlayer src={props.phonetic.audio} controls /> */}
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
